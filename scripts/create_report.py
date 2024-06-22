ffdd7de8da20147751e19a29830b50b2d69979ba
Armando Bringas <armando.bringas@gmail.com>
2024-06-22 16:48:25 -0600

    fix: proyect refactors and re-train of model

--- scripts/create_report.py
+++ scripts/create_report.py
@@ -8,11 +8,20 @@ def create_notebook(script_paths, notebook_paths, output_path):
 
     # Cover Page
     cover_page = nbf.v4.new_markdown_cell(
-        "# Project Report\n\n"
-        "**Author:** Armando Bringas\n\n"
-        "**Date:** June 22, 2024\n\n"
-        "**Project Title:** Behavioral Cloning for Autonomous Driving\n\n"
-        "**Description:** This report includes the scripts and detailed explanations of the behavioral cloning project for autonomous driving."
+        "# Informe del Proyecto\n\n"
+        "**Integrantes**\n\n"
+        "- Armando Bringas Corpus (A01200230).\n"
+        "- Juan Sebastián Téllez López (A01793859).\n"
+        "- Marcos Eduardo García Ortiz (A01276213).\n\n"
+        "**EQUIPO 13**\n\n"
+        "**MR4010 Navegación Autónoma**\n\n"
+        "**Docentes**\n\n"
+        "- Dr. David Antonio Torres.\n"
+        "- Mtra. Maricarmen Vázquez Rojí.\n"
+        "- Tutor Prof. Luis Ángel Lozano Medina"
+        "**Fecha:** 22 de junio de 2024\n\n"
+        "**Título del Proyecto:** Clonación de Comportamiento para Conducción Autónoma\n\n"
+        "**Descripción:** Este informe incluye los scripts y explicaciones detalladas del proyecto de clonación de comportamiento para conducción autónoma.\n\n"
     )
     nb.cells.append(cover_page)
 
@@ -22,7 +31,7 @@ def create_notebook(script_paths, notebook_paths, output_path):
             script_content = file.read()
         script_cell = nbf.v4.new_code_cell(script_content)
         description_cell = nbf.v4.new_markdown_cell(
-            f"## Script: {os.path.basename(script_path)}\n\nDescription of the script.")
+            f"## Script: {os.path.basename(script_path)}\n\nDescripción del script.")
         nb.cells.append(description_cell)
         nb.cells.append(script_cell)
 
@@ -50,10 +59,10 @@ script_paths = [
 ]
 
 notebook_paths = [
-    "behavioral_cloning_training.ipynb"
+    "../notebooks/behavioral_cloning_training.ipynb"
 ]
 
-output_path = "Proyecto_Final_Equipo13.ipynb"
+output_path = "../notebooks/Proyecto_Final_Equipo13.ipynb"
 create_notebook(script_paths, notebook_paths, output_path)
 
 # Convert the notebook to PDF
